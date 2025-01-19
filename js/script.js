1ca4f0cc89a7a3479b9ffa15c5f88d9e6ea9a9c2
maribanez97 <maria.ibanez1997@alumnos.cei.es>
2025-01-19 12:34:41 +0100

    update html validator

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //-----------------------------------
 // 1. Constantes y variables
 //-----------------------------------
-const cursor = document.getElementById("CursorId");
+const cursor = document.getElementById("Cursor");
 const btnMenuMobile = document.getElementById("BtnMenu-mobile");
 const btnMenuDesk = document.getElementById("BtnMenu-desk");
 const divMenu = document.getElementById("Menu");
