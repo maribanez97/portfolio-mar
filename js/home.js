c323e52e8ed6ec45a37e7b5f9a25b5d58bc760a6
maribanez97 <maria.ibanez1997@alumnos.cei.es>
2026-09-22 12:29:37 +0200

    Include project index in titles for display
    Added project index number to project titles in the list.

--- js/home.js
+++ js/home.js
@@ -45,7 +45,8 @@ function imprimirProyectos(proyectos){
     // Limpiar lista de proyectos para no duplicar cuando se filtra por web o gráfico
     ulProyectos.innerHTML = "";
 
-    proyectos.forEach((proyecto) => {
+    proyectos.forEach((proyecto, index) => {
+        let numProyecto = String(index + 1).padStart(2, "0");
         let titulo = proyecto.titulo.toUpperCase();
         let imagen1 = proyecto.imagen1;
         let alt1 = proyecto.alt1;
@@ -58,7 +59,7 @@ function imprimirProyectos(proyectos){
         if (video){
             ulProyectos.innerHTML += `<li class="Proyecto">
                                         <a class="EstiloAnchor Proyecto-enlace" href="./proyecto.html?id=${proyecto.id}">
-                                            <h3 class="Proyecto-titulo">${titulo}</h3>
+                                            <h3 class="Proyecto-titulo">${numProyecto}${titulo}</h3>
                                             <div class="Proyecto-videos">
                                                 <video class="Proyecto-video" src="./videos/${video}" type="video/mp4" poster="./imgs/${imagen2}" autoplay muted loop>
                                                 </video>
@@ -68,7 +69,7 @@ function imprimirProyectos(proyectos){
         } else {
                 ulProyectos.innerHTML += `<li class="Proyecto">
                                             <a class="EstiloAnchor Proyecto-enlace" href="./proyecto.html?id=${proyecto.id}">
-                                                <h3 class="Proyecto-titulo">${titulo}</h3>
+                                                <h3 class="Proyecto-titulo">${numProyecto}${titulo}</h3>
                                                 <div class="Proyecto-imgs">
                                                     <img class="Proyecto-img Proyecto-imgPrincipal" src="./imgs/${imagen1}" loading="lazy" alt="${alt1}">
                                                     <img class="Proyecto-img Proyecto-imgSecundaria" src="./imgs/${imagen2}" loading="lazy" alt="${alt2}">
