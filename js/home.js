87c1ad0fb4b614950de8f89155e57ebc009b6f11
maribanez97 <maria.ibanez1997@alumnos.cei.es>
2026-09-22 13:36:21 +0200

    Refactor project ID formatting in home.js
    Update project number formatting to use project ID.

--- js/home.js
+++ js/home.js
@@ -45,8 +45,8 @@ function imprimirProyectos(proyectos){
     // Limpiar lista de proyectos para no duplicar cuando se filtra por web o gráfico
     ulProyectos.innerHTML = "";
 
-    proyectos.forEach((proyecto, id) => {
-        let numProyecto = String(id + 1).padStart(2, "0");
+    proyectos.forEach((proyecto) => {
+        let numProyecto = String(proyecto.id).padStart(2, "0");
         let titulo = proyecto.titulo.toUpperCase();
         let imagen1 = proyecto.imagen1;
         let alt1 = proyecto.alt1;
